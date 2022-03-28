4ff1b4648731a2815a74c1d69ea958fb472e1727
吴泽辉 <wuzehui@flashexpress.com>
2022-03-28 13:22:48 +0800

    update

--- w02/main.go
+++ w02/main.go
@@ -8,29 +8,25 @@ import (
 	"log"
 )
 
-// getName 模拟dao层方法
-// 如果没有查到数据，返回wrap的sql.ErrNoRows，提供原生sql
-func getName(id int, name *string) error {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
+// NameNotFound 包装底层的错误
+var NameNotFound = fmt.Errorf("name not found %w", sql.ErrNoRows)
 
-	querySql := "select name from t1 where id = ?"
-	err = db.QueryRow(querySql, id).Scan(name)
+// getName 模拟dao层方法
+// 如果没有查到数据，返回wrap的错误变量
+func getName(id int) error {
+	err := sql.ErrNoRows
 	if err == sql.ErrNoRows {
-		return fmt.Errorf("%w, sql: %s, bind: %d", err, querySql, id)
+		return NameNotFound
 	}
 
 	return err
 }
 
 func main() {
-	var name string
 	// 模拟业务调用
-	err := getName(100, &name)
-	if errors.Is(err, sql.ErrNoRows) {
+	err := getName(100)
+	// 这里使用我们的错误来处理，好处是对持久化的底层实现没有依赖
+	if errors.Is(err, NameNotFound) {
 		fmt.Println(err)
 		return
 	}
@@ -38,6 +34,4 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	fmt.Println(name)
 }
