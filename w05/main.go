e6218e2f4da89cca19100fc55ed5f83442240bfe
wuzehv <wuzehv@qq.com>
2022-04-18 00:37:56 +0800

    update

--- w05/main.go
+++ w05/main.go
@@ -2,38 +2,72 @@ package main
 
 import (
 	"fmt"
-	"sync/atomic"
+	"sync"
 	"time"
 )
 
-const (
-	// vLen 滑动窗口统计保留数量
-	vLen = 10
-)
+// rollingNumber 数字载体，用简单类型
+type rollingNumber int64
 
-// diff 为当前时间戳与长度的取模，用来保证数组下标从0开始
-var diff = time.Now().Unix() % vLen
+// rollingNumberContainer 整个滑动窗口的计数容器，采用map结构
+type rollingNumberContainer map[int64]*rollingNumber
 
-type rollingNumber [vLen]int64
-
-func (h *rollingNumber) getKey(t int64) int64 {
-	return (t - diff) % vLen
+// container 滑动窗口，均使用非导出字段
+type container struct {
+	lock sync.RWMutex
+	// size 滑动窗口中统计的数量；每秒一个容器
+	size int64
+	rn   rollingNumberContainer
 }
 
-func (h *rollingNumber) Incr(t int64) {
-	atomic.AddInt64(&h[h.getKey(time.Now().Unix())], t)
+// getNumber 获取底层计数
+func (h *container) getNumber() *rollingNumber {
+	key := time.Now().Unix()
+	if _, ok := h.rn[key]; !ok {
+		var tmp rollingNumber
+		h.rn[key] = &tmp
+	}
+
+	return h.rn[key]
 }
 
-type HystrixContainer struct {
-	all       *rollingNumber
-	successes *rollingNumber
-	failures  *rollingNumber
-	timeouts  *rollingNumber
+// setNumber 设置数字
+func (n *rollingNumber) setNumber(num int64) {
+	*n += rollingNumber(num)
 }
 
+// removeItem 移除过期的容器
+func (h *container) removeItem() {
+	diff := time.Now().Unix() - h.size
+	for k := range h.rn {
+		if k <= diff {
+			delete(h.rn, k)
+		}
+	}
+}
+
+// Incr 增加
+func (h *container) Incr(t int64) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	x := h.getNumber()
+	x.setNumber(t)
+	h.removeItem()
+}
+
+func NewContainer(size uint) *container {
+	return &container{size: int64(size), rn: make(rollingNumberContainer, 0)}
+}
 
 func main() {
-	x := &rollingNumber{}
+	x := NewContainer(10)
+
+	go func() {
+		for {
+			x.Incr(1)
+			time.Sleep(1 * time.Millisecond)
+		}
+	}()
 
 	go func() {
 		for {
@@ -42,6 +76,8 @@ func main() {
 		}
 	}()
 
-	time.Sleep(25 * time.Second)
-	fmt.Println(x)
+	time.Sleep(3 * time.Second)
+	for k, v := range x.rn {
+		fmt.Printf("key: %v, number: %d\n", k, *v)
+	}
 }
